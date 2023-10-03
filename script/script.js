4d8023039db40afbef8f917845dc0885abcc39c0
azzudin1108 <82307614+azzudin1108@users.noreply.github.com>
2023-10-03 18:33:05 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const content = document.querySelector('#content');
 const container = content.querySelector('.container');
 const selection = container.getElementsByTagName('button');
 
-let sayang = true;
+let sayang = null;
 
 function del() {
     selection[1].classList.remove('clicked');
@@ -34,9 +34,22 @@ container.addEventListener('click', function (e) {
 
 content.addEventListener('click', function (e) {
     if (e.target.getAttribute('type') == 'submit') {
-        e.target.classList.add('clicked');
-        setInterval(del2, 1000);
-        document.querySelector('.hore').style.display = 'flex';
-        setInterval(dNone, 1000);
+        if (sayang == true) {
+            e.target.classList.add('clicked');
+            setInterval(del2, 1000);
+            document.querySelector('.hore').style.display = 'flex';
+        } else if (sayang == null) {
+            alert('Pilih dulu, sayang!!!');
+        }
+    }
+});
+
+document.querySelector('.hore').addEventListener('click', function (e) {
+    if (e.target.getAttribute('id') == 'back') {
+        e.target.parentElement.style.display = 'none';
+        sayang = null;
+        for (let i = 0; i < selection.length; i++) {
+            selection[i].classList.remove('clicked');
+        }
     }
 });
