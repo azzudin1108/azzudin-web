ae862173e75061f91aa77310391adabe1c986ef4
azzudin1108 <82307614+azzudin1108@users.noreply.github.com>
2023-10-04 19:49:50 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,55 +1,58 @@
-const content = document.querySelector('#content');
-const container = content.querySelector('.container');
-const selection = container.getElementsByTagName('button');
+const score = document.querySelector('.point-score');
+const soal = document.querySelectorAll('.soal');
+const ekspresi = document.querySelector('#ekspresi');
+const tryAgain = document.getElementById('tryAgain');
+const nomor = document.getElementById('nomor');
 
-let sayang = null;
 
-function del() {
-    selection[1].classList.remove('clicked');
-    selection[0].classList.add('clicked');
-}
+let point = 0;
+let noSoal = 1;
+score.innerHTML = '<h2>' + point + '</h2>';
+nomor.innerHTML = noSoal;
 
-function del2() {
-    content.getElementsByTagName('button')[2].classList.remove('clicked');
-}
-
-function dNone() {
-    document.querySelector('.hore').style.display = 'none';
-}
-
-container.addEventListener('click', function (e) {
-    for (let i = 0; i < selection.length; i++) {
-        selection[i].classList.remove('clicked');
-    }
-    if (e.target.getAttribute('type') == 'menu') {
-        if (e.target.value == 1) {
-            selection[1].classList.add('clicked');
-            setInterval(del, 1000);
-        } else {
+for (let i = 0; i < soal.length; i++) {
+    soal[i].addEventListener('click', function (e) {
+        if (e.target.getAttribute('type') == 'menu') {
+            for (let btnIndex = 0; btnIndex < soal[0].getElementsByTagName('button').length; btnIndex++) {
+                soal[i].getElementsByTagName('button')[btnIndex].classList.remove('clicked');
+            }
+            if (e.target.value == 1) {
+                point += 10;
+            } else {
+                point += 0;
+            }
             e.target.classList.add('clicked');
+        } else if (e.target.getAttribute('type') == 'submit') {
+            if (e.target.parentElement.classList.contains('last')) {
+                ekspresi.parentElement.style.display = 'flex';
+                if (point <= 30) {
+                    ekspresi.innerHTML = 'Silitttt!!!!!'
+                } else if (point <= 50) {
+                    ekspresi.innerHTML = 'Huuuuu!!!!!';
+                } else if (point <= 90) {
+                    ekspresi.innerHTML = 'Belajar lagi, dekkkk!!!!!';
+                } else {
+                    ekspresi.innerHTML = 'Waduh, Tumbennnn!!!!!';
+                }
+            } else {
+                e.target.parentElement.nextElementSibling.style.display = 'flex';
+            }
+            e.target.parentElement.style.display = 'none';
+            score.innerHTML = '<h2>' + point + '</h2>';
+            noSoal += 1;
+            nomor.innerHTML = noSoal;
+            for (let a = 0; a < soal[i].querySelectorAll('button[type=menu]'); a++) {
+                soal[i].querySelectorAll('button[type=menu]')[a].classList.remove('clicked');
+            }
         }
-    }
-    return sayang = true;
-});
+    });
+}
 
-content.addEventListener('click', function (e) {
-    if (e.target.getAttribute('type') == 'submit') {
-        if (sayang == true) {
-            e.target.classList.add('clicked');
-            setInterval(del2, 1000);
-            document.querySelector('.hore').style.display = 'flex';
-        } else if (sayang == null) {
-            alert('Pilih dulu, sayang!!!');
-        }
-    }
-});
-
-document.querySelector('.hore').addEventListener('click', function (e) {
-    if (e.target.getAttribute('id') == 'back') {
-        e.target.parentElement.style.display = 'none';
-        sayang = null;
-        for (let i = 0; i < selection.length; i++) {
-            selection[i].classList.remove('clicked');
-        }
-    }
+tryAgain.addEventListener('click', function () {
+    soal[0].style.display = 'flex';
+    ekspresi.parentElement.style.display = 'none';
+    point = 0;
+    score.innerHTML = '<h2>' + point + '</h2>';
+    noSoal = 1;
+    nomor.innerHTML = noSoal;
 });
